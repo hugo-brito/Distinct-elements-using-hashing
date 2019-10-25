2758da647307578308cb0f751a49d82fe9f24a9e
Hugo de Brito <hubr@itu.dk>
2019-10-25 11:01:26 +0200

    Ex 3. passes codejudge

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,8 +1,144 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Ex3 {
 
-//	for i:=0 to m-1 do M[i]:=0
+	private static int[] A = {
+	// Needed for the hashing function
+			0x21ae4036,
+			0x32435171,
+			0xac3338cf,
+			0xea97b40c,
+			0x0e504b22,
+			0x9ff9a4ef,
+			0x111d014d,
+			0x934f3787,
+			0x6cd079bf,
+			0x69db5c31,
+			0xdf3c28ed,
+			0x40daf2ad,
+			0x82a5891c,
+			0x4659c7b0,
+			0x73dc0ca8,
+			0xdad3aca2,
+			0x00c74c7e,
+			0x9a2521e2,
+			0xf38eb6aa,
+			0x64711ab6,
+			0x5823150a,
+			0xd13a3a9a,
+			0x30a5aa04,
+			0x0fb9a1da,
+			0xef785119,
+			0xc9f0b067,
+			0x1e7dde42,
+			0xdda4a7b2,
+			0x1a1c2640,
+			0x297c0633,
+			0x744edb48,
+			0x19adce93};
+
+	private int m;
+	private int M[];
+	private int V;
+	private double Z;
+
+	Ex3 (int m) {
+		if (!isPowerOfTwo(m)) throw new InputMismatchException("m: " + m + "\nThe provided m is not a power of 2!");
+		this.m = m;
+		this.V = m; // # of empty buckets
+		this.M = new int[m];
+	}
+
+	Ex3 () {
+		this(1024);
+	}
+
+	private int h (int x) {
+	// The hashing function
+		int res = 0;
+		for (int i = 0; i < A.length; i++) res += (Integer.bitCount(A[i] & x) % 2) << (31-i);
+		return res;
+	}
+
+	private int rho (int x) {
+		if (x == 0) throw new InputMismatchException("Zero has no leading zeroes.");
+		return Integer.numberOfLeadingZeros(x) + 1;
+	}
+
+	private int f (int y) {
+		return ((y*0xbc164501) & 0x7fe00000) >> 21;
+		// 21 should be a function of the size of m.
+		// 1024 -> 21
+	}
+
+	public void addToM(int x) {
+		int j = f(x);
+		if (M[j] == 0) V--;
+		M[j] = Math.max(M[j], rho(h(x)));
+	}
+
+	private void calculateZ () {
+		//	Harmonic Mean
+		double res = 0.0;
+		for (int i = 0; i < m; i++) res = res + Math.pow(2.0,-M[i]);
+		System.out.println();
+		Z = 1.0/res;
+	}
+
+	private int naiveV () {
+		// # of empty buckets
+		int V = 0;
+		for (int i = 0; i < m; i++) if (M[i] == 0) V++;
+		return V;
+	}
+
+	public double E () {
+		calculateZ();
+		// Estimation of the number of distinct elements
+		double E = (m * m * Z * 0.7213)/(1.0 + (1.079/m));
+//		if (E < 2.5*m and V > 0) then E:= m * ln(m/V)
+		System.err.println("E before if statement:\n" + E);
+		System.err.println("\nV in the field:\n" + V);
+		System.err.println("\nNaive V:\n" + naiveV());
+		if ((E < (2.5 * m)) && (V > 0)) {
+			E = ((double) m) * Math.log(((double)m)/((double)V));
+		}
+		// some problem here
+		return E;
+
+	}
+
+	static boolean isPowerOfTwo (int n) {
+		if(n==0) return false;
+		return (int)(Math.ceil((Math.log(n) / Math.log(2)))) == (int)(Math.floor(((Math.log(n) / Math.log(2)))));
+	}
+
+	public static void main(String[] args) {
+		Ex3 ex3 = new Ex3();
+
+		Scanner sc = new Scanner(System.in);
+		int threshold = sc.nextInt();
+		while (sc.hasNext()) {
+			ex3.addToM(sc.nextInt());
+		}
+
+		System.err.println(ex3.V == ex3.naiveV());
+
+		double E = ex3.E();
+
+		System.err.println(E);
+
+		System.out.println(E > threshold ? "above" : "below");
+
+		//	for i:=0 to m-1 do M[i]:=0
+		// the size of m is a parameter of the algorithm
+		// the larger the more precise
+
 //	for i:=1 to n do
-//	    j := f(y[i])
+		// another parameter of the algorithm: the number of intergers
+
+// j := f(y[i])
 //	    M[j] := max(M[j],rho(h(y[i])))
 //	end
 //	Z := 1/(2^(-M[0])+...+2^(-M[m-1]))
@@ -10,4 +146,5 @@ public class Ex3 {
 //	E := m*m*Z*0.7213/(1 + 1.079/m)
 //			if (E < 2.5*m and V > 0) then E:= m * ln(m/V)
 //	return E
+	}
 }
